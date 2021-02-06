db19c57e64488ae071250453aa7abf85c3b59e76
Varun Goyal <varungoyal23@gmail.com>
2021-02-07 01:26:44 +0530

    test 2 updated

--- tests/test2.c
+++ tests/test2.c
@@ -2,5 +2,9 @@ char a=    'a'  ;
 /* hello char testin // char a = 34 
 //single line inside multi line
 /* multi line inside multi line */
-a =  45;
+a =  12 34 56; ; 12 ; 34; a; 45;
+while wHile
+for For fOR fOr
+int b =    067576; // Octal number put into an integer
+c c = 54;
 
