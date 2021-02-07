64b077604fcd324800e3d8667385afdaffb7607c
lavish315 <guptalavish315@gmail.com>
2021-02-07 10:29:03 +0530

    some operators to handle. A new test case added

--- src/lexer.py
+++ src/lexer.py
@@ -3,7 +3,8 @@ import ply.lex as lex
 from ply.lex import TOKEN
 from tabulate import tabulate
 
-# some symbols to add
+# some symbols to add : pointer operator, dot operator, ternary operator
+# alternate curly, square brackets, adding ignore characters (\v , \f)
 
 keyword_tokens = {}
 
---
+++ tests/test3.c
@@ -0,0 +1,10 @@
+int main(){
+    int a = 5, *b = &a , intt ;
+    int d = b->a ;
+    int e = b.a ;
+    if(true)
+    {
+        int e = (a > intt) ? a : b ;
+    }
+    return ;
+}
