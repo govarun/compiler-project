d7d3cbf675b57e65209342e425835c83a908cc4b
lavish315 <guptalavish315@gmail.com>
2021-02-07 12:49:24 +0530

    checking array and pointers

---
+++ tests/arrays_pointers.c
@@ -0,0 +1,26 @@
+typedef struct point{
+    int x ;
+    int y ;
+}   ;
+
+int main()
+{
+    point p[5] ;
+    int arr[10] ;
+    for(int i = 0 ; i < 10 ; i++)
+    {
+        arr[i] = i ;
+    }
+    for(int i = 0 ; i < 5 ; i+=2)
+    {
+        p[i].x = arr[2*i] ;
+        p[i].y = arr[2*i + 1] ;
+    }
+
+    point *q = &p, *r = &p[0] , *s = &p[1] ;
+    int *x = &(arr + 2) ; 
+    *x = 5 ;
+    int **z = &(x) ;
+    *(*z) = 10 ;
+    return ;
+}
