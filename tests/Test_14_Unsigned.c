1be71c352a680253d12008d68d04b4df4e9abb68
lavish315 <guptalavish315@gmail.com>
2021-02-08 19:44:36 +0530

    suffix test_case added

---
+++ tests/Test_14_Unsigned.c
@@ -0,0 +1,31 @@
+int main()
+{
+    {
+        long long a = 100LL ;
+        long b = 100L ;
+    }
+    {
+        long long a = 100ll ;
+        long b = 100l ;
+        long long c = 100Ll ;
+        long long d = 100lL ;
+    }
+    {
+        unsigned long long a = 100ULL ;
+        unsigned long b = 100UL ;
+    }
+    {
+        unsigned long long a = 100uLL ;
+        unsigned long b = 100uL ;
+    }
+    {
+        unsigned long long a = 100Ull ;
+        unsigned long b = 100Ul ;
+    }
+    {
+        unsigned long long a = 100ull ;
+        unsigned long b = 100ul ;
+        unsigned long long c = 100ulL ;
+        unsigned long long d = 100uLl ;
+    }
+}
