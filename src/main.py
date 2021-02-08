9e93070e9e65dc2934d2747bc9c0bb08f81fe8c0
sartkap <sarthakkapoor76@gmail.com>
2021-02-08 14:56:13 +0530

    --help and requirements.txt added

--- src/main.py
+++ src/main.py
@@ -2,10 +2,19 @@ import lexer
 import sys
 s = str(sys.argv)
 if(len(sys.argv) <= 1):
-	print("No file specified")
+	print("Incorrect Usage")
+	print("Correct Usage: python3 main.py [--help] | [file names seperated by space]")
 	exit()
-# print(sys.argv[1])
-
+if(sys.argv[1] == "--help"):
+	print('Usage: python3 main.py [--help | file names seperated by space]')
+	print('Options-')
+	print('--help : \t Small tutorial on how to use the compiler\n')
+	print('Dependencies:')
+	print('This compiler makes use of the following python3 packages:')
+	print('1. ply')
+	print('2. tabulate\n')
+	print('To install these dependencies, simply run "pip install -r requirements.txt"(without quotes) from the root of the repo')
+	exit()
 for i in range(len(sys.argv)-1):
 	file = open(sys.argv[i+1])
 	code = file.read()
