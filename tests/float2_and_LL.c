78d65ad68beb1b73330388750d1eae3bc4b3212a
lavish315 <guptalavish315@gmail.com>
2021-02-08 15:22:32 +0530

    checks exponential and LL

---
+++ tests/float2_and_LL.c
@@ -0,0 +1,27 @@
+int main()
+{
+    {
+        int a = 1e5 , b = 1e-5 ;
+        float c = 1e5 , d = 1e-5 , e = -1e5 , f = -1e-5 ;
+        float g = -1.2e-5 , h = -1.2e5 , i = -1.2e+5 ;
+    }
+    {
+        int a = 1E5 , b = 1E-5 ;
+        float c = 1E5 , d = 1E-5 , e = -1E5 , f = -1E-5 ;
+        float g = -1.2E-5 , h = -1.2E5 , i = -1.2E+5 ;
+    }
+    {
+        // valid
+        float a = 0x5e2 ; 
+        float b = 0x5ea ;
+    }
+    {
+        float a = 1.2e5.0 , b = 1.2e5.1 ; // invalid
+    }
+    {
+        long long a = (1LL) ;
+        long long b = (1ll) ;
+        long long c = (LL1) ; // invalid
+        long long d = (1LL << 50) ;
+    }
+}
