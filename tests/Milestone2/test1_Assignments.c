2f4745dcf75e8b8723b9cbade07ffd383de53342
Prakhar Neema <67539999+nprakhar0411@users.noreply.github.com>
2021-03-09 19:22:32 +0530

    Update and rename test_file to test1_Assignments.c

---
+++ tests/Milestone2/test1_Assignments.c
@@ -0,0 +1,9 @@
+int main(){
+    int randomName = 0;
+    double main;
+    char character;
+    char char; // No syntax Error.
+    // char a, int;   // Syntax Error.
+    long array[10];
+    return 0;
+}
