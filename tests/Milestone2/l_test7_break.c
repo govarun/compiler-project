36ada28f8d068b9db507684eab77185e3538c147
lavish315 <guptalavish315@gmail.com>
2021-03-09 22:39:47 +0530

    test cases added

---
+++ tests/Milestone2/l_test7_break.c
@@ -0,0 +1,16 @@
+int main()
+{
+    int a ;
+    for(a = 1 ; a <= 10 && a >= 10; a++)
+    {
+        if(a > 5)
+            break ;
+        break ;
+    }
+    if(a < 10)
+    {
+        a++ ;
+        break ;
+    }
+    return 0 ;
+}
---
+++ tests/Milestone2/l_test8_brackets.c
@@ -0,0 +1,18 @@
+int main()
+{
+    {
+        int a = 5 ;
+        a++ ;
+        //int b = 6 ; // this will give error
+    }
+    {
+        int a = 6 ;
+        if({b == 6})
+        {
+            {} ;
+        }
+    }
+    if(a == 5))
+        ;
+    return 0 ;
+}
