0bd827f25ef9209d923c840537838e8a9d4b924f
lavish315 <guptalavish315@gmail.com>
2021-03-09 22:13:49 +0530

    test cases added

---
+++ tests/Milestone2/l_test4_structs.c
@@ -0,0 +1,10 @@
+struct point{
+    int x ;
+    long int y ;
+};
+int main()
+{
+    struct point a ;
+    //point b ; // This is giving syntax error
+    return 1 ;
+}
---
+++ tests/Milestone2/l_test5_typedef.c
@@ -0,0 +1,10 @@
+typedef struct point{
+    int x ;
+    long int y ;
+} point ;
+int main()
+{
+    //struct point a ;
+    point b ; // This is giving syntax error
+    return 1 ;
+}
