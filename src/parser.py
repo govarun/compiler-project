d63b598b31b31665c00ae6d46fa510cfd9221c46
Jayesh Shaw <jayeshaw@iitk.ac.in>
2021-03-27 17:01:27 +0530

    Parameters pushed into symbol table

--- src/parser.py
+++ src/parser.py
@@ -870,14 +870,12 @@ def p_parameter_list(p):
                       | parameter_list COMMA parameter_declaration
     '''
     #p[0] = Node()
+    p[0] = Node(name = 'ParameterList', val = '', type = '', children = [], lno = p.lineno(1))
     if(len(p) == 2):
-      p[0] = p[1]
-      p[0].name = 'ParameterList'
-      p[0].children = p[1]
+      p[0].children.append(p[1])
     else:
-      p[0] = p[1]
+      p[0].children = p[1].children
       p[0].children.append(p[3])
-      p[0].name = 'ParameterList'
 
 def p_parameter_declaration(p):
     '''parameter_declaration : declaration_specifiers declarator
@@ -890,6 +888,15 @@ def p_parameter_declaration(p):
       p[0].name = 'ParameterDeclaration'
     else:
       p[0] = Node(name = 'ParameterDeclaration',val = '',type = p[1].type, lno = p[1].lno, children = [])
+    if(p[2].name == 'Declarator'):
+      if(p[2].val in symbol_table[currentScope].keys()):
+        print(p.lineno(1), 'COMPILATION ERROR : ' + p[2].val + ' parameter already declared')
+      symbol_table[currentScope][p[2].val] = {}
+      symbol_table[currentScope][p[2].val]['type'] = p[1].type
+      if(len(p[2].type) > 0):
+        symbol_table[currentScope][p[2].val]['type'] = p[1].type + ' ' + p[2].type
+      if(len(p[2].array) > 0):
+        symbol_table[currentScope][p[2].val]['array'] = p[2].array
 
 
 def p_identifier_list(p):
@@ -1183,7 +1190,7 @@ def runmain(code):
   graph.write_png('pydot_graph.png')
 
 def visualize_symbol_table():
-  for i in range (1,nextScope):
+  for i in range (nextScope):
     if(len(symbol_table[i]) > 0):
       print('\nIn Scope ' + str(i))
       for key in symbol_table[i].keys():
