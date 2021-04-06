0bf918fd9c0bd391adc260fa05553f08f9cd2eae
Prakhar Neema <67539999+nprakhar0411@users.noreply.github.com>
2021-04-06 14:41:20 +0530

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -1069,7 +1069,6 @@ def p_parameter_declaration(p):
         print(p.lineno(1), 'COMPILATION ERROR : ' + p[2].val + ' parameter already declared')
       symbol_table[currentScope][p[2].val] = {}
       symbol_table[currentScope][p[2].val]['type'] = p[1].type
-			symbol_table[currentScope][p[2].val]['size'] = get_data_type_size(p[1].type)	
       if(len(p[2].type) > 0):
         symbol_table[currentScope][p[2].val]['type'] = p[1].type + ' ' + p[2].type
       if(len(p[2].array) > 0):
