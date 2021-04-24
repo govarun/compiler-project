a57e7e89841ff3cb6dc0db1c7ae8e1198256382c
Varun Goyal <varungoyal23@gmail.com>
2021-04-24 22:47:10 +0530

    register functions declared

---
+++ src/reg_funcs.py
@@ -0,0 +1,20 @@
+
+
+def free_all_regs():
+    pass
+
+def get_register(instr_3ac, compulsory = True, exclude_reg = []):
+    if compulsory == False:
+        print("don't call me when not needed")
+    pass
+
+def save_reg_to_mem():
+    pass
+
+def get_location_mem():
+    pass
+
+def get_best_location():
+    pass
+
+
