5eb9de8bc0bdd8d10d843cd7e83f494abfdd4eac
Varun Goyal <varungoyal23@gmail.com>
2021-04-26 02:04:19 +0530

    some reg func changes

--- src/reg_funcs.py
+++ src/reg_funcs.py
@@ -1,9 +1,20 @@
+from helper_functions import *
+
+# 32 bit register descriptors
+reg_desc = {}
+reg_desc["eax"] = set()
+reg_desc["ebx"] = set()
+reg_desc["ecx"] = set()
+reg_desc["edx"] = set()
+reg_desc["esi"] = set()
+reg_desc["edi"] = set()
 
 
-def free_all_regs():
+def free_all_regs(instr):
+    # src1
     pass
 
-def get_register(instr_3ac, compulsory = True, exclude_reg = []):
+def get_register(instr_3ac, compu'lsory = True, exclude_reg = []):
     src1 = instr_3ac[1]
     src2 = instr_3ac[2]
     dest = instr_3ac[3]
