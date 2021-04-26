680c98fb3bf9673f95bc5a94abff83f7792f0679
destroyer <sarthakkapoor76@gmail.com>
2021-04-26 18:36:26 +0530

    op add done

--- src/codegen.py
+++ src/codegen.py
@@ -1,4 +1,5 @@
 from parser import symbol_table
+from reg_funcs import *
 class CodeGen:
     def gen_top_headers(self):
         print("section .text")
@@ -10,6 +11,15 @@ class CodeGen:
         for vars in symbol_table[0].keys():
             print(vars + "\tdd\t0")
 
+    def add(self,quad):
+        #check where moved back into memory
+        reg1 = get_register(quad)
+        if(not reg1.startswith("[")):
+            print("\tmov " + reg1 + ", " + get_best_location(quad.src1))
+        reg2 = get_best_location(quad.src2)
+        print("\tadd " + reg1 + ", " + reg2)
+        upd_reg_desc(reg1,quad.dest)
+        free_all_regs(quad)
 
 def runmain():
     codegen = CodeGen()
