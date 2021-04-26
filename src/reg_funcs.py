f4a99052fc52601206c3e7294b2e0b7a2c182f89
Jayesh Shaw <jayeshaw@iitk.ac.in>
2021-04-26 12:37:15 +0530

    Added symbol info table

---
+++ src/reg_funcs.py
@@ -0,0 +1,33 @@
+from helper_functions import *
+
+# 32 bit register descriptors
+reg_desc = {}
+reg_desc["eax"] = set()
+reg_desc["ebx"] = set()
+reg_desc["ecx"] = set()
+reg_desc["edx"] = set()
+reg_desc["esi"] = set()
+reg_desc["edi"] = set()
+
+
+def free_all_regs(instr):
+    # src1
+    pass
+
+def get_register(instr_3ac, compu'lsory = True, exclude_reg = []):
+    src1 = instr_3ac[1]
+    src2 = instr_3ac[2]
+    dest = instr_3ac[3]
+    
+def save_reg_to_mem():
+    pass
+
+def get_location_mem():
+    pass
+
+def get_best_location():
+    pass
+
+
+
+
