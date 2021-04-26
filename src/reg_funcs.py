e1db890cc1fe302afa47eabb26b85ae3abf98956
Jayesh Shaw <jayeshaw@iitk.ac.in>
2021-04-26 13:29:35 +0530

    Added some register functions

--- src/reg_funcs.py
+++ src/reg_funcs.py
@@ -14,12 +14,41 @@ def free_all_regs(instr):
     # src1
     pass
 
-def get_register(instr_3ac, compu'lsory = True, exclude_reg = []):
-    src1 = instr_3ac[1]
-    src2 = instr_3ac[2]
-    dest = instr_3ac[3]
+def get_register(instr, compulsory = True, exclude_reg = []):
     
-def save_reg_to_mem():
+    for reg in symbol_info[instr.src1].address_desc_reg:
+        if reg not in exclude_reg:
+            if(len(reg_desc[reg]) == 1 and instr.instr_info['nextuse'][instr.src1] == None\
+             and not instr.instr_info['live'][instr.src1]):
+                symbol_info[instr.src1].address_desc_reg.remove(reg)
+                return reg
+
+    for reg in reg_desc.keys():
+        if(reg not in exclude_reg):
+            if(len(reg_desc[reg]) == 0):
+                return reg
+    
+    if(instr.instr_info['nextuse'][instr.dest] != None or compulsory = True):
+        R = None
+        for reg in reg_desc.keys():
+            if(reg not in exclude_reg):
+            if(R == None):
+                R = reg   
+            elif(len(reg_desc[reg]) < len(reg_desc[R])):
+                R = reg
+        return R
+
+    else:
+        return get_location_mem(instr.dest)
+
+        
+
+    
+def save_reg_to_mem(reg):
+    saved_loc = set()
+    for symbol in reg_desc[reg]:
+        for(location in symbols[symbol].address_desc_mem):
+
     pass
 
 def get_location_mem():
