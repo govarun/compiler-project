67a9f6cb91bd25396096583ce644d5fad6cb9726
Prakhar Neema <prakharneema1999@gmail.com>
2021-04-28 01:25:47 +0530

    lshift and rshift handled

--- src/codegen.py
+++ src/codegen.py
@@ -118,8 +118,41 @@ class CodeGen:
         if check_type_location(best_location) == "register":
             upd_reg_desc(best_location, quad.dest)
         print("\tneg " + best_location)
+
+
     def lshift(self, quad):
-        pass
+        best_location = get_best_location(quad.src1)
+        reg1 = get_register(quad, compulsory=True)
+        upd_reg_desc(reg1, quad.dest)
+        if best_location != reg1:
+            print("\tmov " + reg1 + ", " + best_location)
+        
+        best_location = get_best_location(quad.src2)
+        if check_type_location(best_location) == "number":
+            print("\tshl " + reg1 +  ', ' + best_location)
+        else:
+            if best_location != "ecx":
+                upd_reg_desc("ecx",quad.src2)
+                print("\tmov " + "ecx" + ", " + best_location)
+            print("\tshl " + reg1 + ", cl")
+        free_all_regs(quad)
+
+    def rshift(self, quad):
+        best_location = get_best_location(quad.src1)
+        reg1 = get_register(quad, compulsory=True)
+        upd_reg_desc(reg1, quad.dest)
+        if best_location != reg1:
+            print("\tmov " + reg1 + ", " + best_location)
+
+        best_location = get_best_location(quad.src2)
+        if check_type_location(best_location) == "number":
+            print("\tshr " + reg1 + ', ' + best_location)
+        else:
+            if best_location != "ecx":
+                upd_reg_desc("ecx", quad.src2)
+                print("\tmov " + "ecx" + ", " + best_location)
+            print("\tshr " + reg1 + ", cl")
+        free_all_regs(quad)
     
     def relational_op(self,quad):
         best_location = get_best_location(quad.src1)
@@ -286,7 +319,6 @@ class CodeGen:
         elif(quad.op.endswith("uminus")):
             self.assign(quad)
             self.uminus(quad)
-
         elif(quad.op.split("_")[-1] in relational_op_list):
             self.relational_op(quad)
         elif(quad.op == "ifgoto"):
@@ -295,6 +327,14 @@ class CodeGen:
             self.label(quad)
         elif(quad.op == "goto"):
             self.goto(quad)
+        elif(quad.op.endswith("<<")):
+            self.lshift(quad)
+        elif(quad.op.endswith(">>")):
+            self.rshift(quad)
+
+
+
+
 def runmain():
     sys.stdout = open('out.asm', 'w')
     codegen = CodeGen()
