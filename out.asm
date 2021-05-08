b97d40ad35a1792605b933c4ecff54fd76e85763
destroyer <sarthakkapoor76@gmail.com>
2021-05-08 22:57:00 +0530

    sizeof added

--- out.asm
+++ out.asm
@@ -7,11 +7,19 @@ section .text
 main:
 	push ebp
 	mov ebp, esp
-	sub esp, 4
-	mov eax, None
-	mov dword [ebp-4], eax
+	sub esp, 16
+	mov dword [ebp-8], 4
+	mov eax, dword [ebp-8]
+	push eax
+	push __t_1
+	mov dword [ebp-12], eax
+	mov dword [ebp-8], eax
+	call printf
+	mov dword [ebp-16], eax
+	add esp, 8
 	mov eax, 0
 	mov esp, ebp
 	pop ebp
 	ret
 section	.data
+	__t_1:	db	`%d`, 0
