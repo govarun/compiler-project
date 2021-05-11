c07a4555e4f28de0c16fff27c25be8e7e300b16a
lavish315 <guptalavish315@gmail.com>
2021-05-11 14:16:31 +0530

    float test cases added

--- Milestone4/trial.c
+++ Milestone4/trial.c
@@ -1,13 +1,39 @@
-float eps = 0.05 ;
-float lim = 0.04 ;
+float eps = 1e-4 ;
+void get_val(float& val, int a , float b , float x)
+{
+    val = (a*x + b) ;
+    return ;
+}
+
+float get_val_quad(int a , int b , int c , float x)
+{
+    return (a*x*x + b*x + c) ;
+}
 int main()
 {
-    float a , b , c , d ;
-    float *e , *f ;
-    a = 1.1 , b = 2.2 , c = 3.3 , d = 4.4 ;
-    e = &b;
-    f = e+1 ;
+    int a = 1 , b = -10 , c = 25 ;
+    float x[100] ;
+    int i = 1 ;
+    int iter = 30;
+    float curr_val_diff , curr_val_func ;
+    x[0] = 0 ;
+    for(i ; i < iter ; i++)
+    {
+        get_val(curr_val_diff , 2*a , b , x[i-1]) ;
+        curr_val_func = get_val_quad(a , b , c , x[i-1]) ;
 
-    printf("e_val = %f , f_val = %f\n", *e , *f) ;
+        x[i] = x[i-1] ;
+        if(curr_val_func < eps && curr_val_func > -eps)
+            continue ;
+        if(curr_val_diff < eps && curr_val_diff > -eps)
+            continue ;
+
+        printf("i = %d, diff = %f, val = %f\n", i ,curr_val_diff , curr_val_func) ;
+        
+        x[i] = x[i-1] - (curr_val_func) / curr_val_diff ;
+    }
+    printf("root = %f\n", x[iter-1]) ;
+
     return 0 ;
+
 }
