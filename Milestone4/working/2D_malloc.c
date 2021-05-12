955307162a0cac9fdf662e8e1a68b772c7c51997
Jayesh Shaw <jayeshaw@iitk.ac.in>
2021-05-12 08:28:52 +0530

    Testcases added and revised

--- Milestone4/working/2D_malloc.c
+++ Milestone4/working/2D_malloc.c
@@ -5,5 +5,16 @@ int main(){
     for(i = 0; i < 4; i++){
         arr[i] = (int*)malloc((i+1)*sizeof(int));
     }
+    for(i = 0; i < 4; i++){
+        for(j = 0; j <= i; j++){
+            arr[i][j] = i+j;
+        }
+    }
+    for(i = 0; i < 4; i++){
+        for(j = 0; j <= i; j++){
+            printf("[%d %d] ", *(*(arr + i) + j), arr[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
